d2681bbf0e20568588876b9af13f076d30f1a9db
penicillin0 <56141035+penicillin0@users.noreply.github.com>
2021-02-20 23:20:59 +0900

    fliter problems (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import { Header } from './components/Header';
 import { Top } from './pages/Top';
 import { getAtCoderStatus } from './api/apiClient';
 import { problemType } from './types/problem';
+import { problems } from './data/constants';
 
 function App() {
   const [userName, setUserName] = React.useState<string>('');
-  const [atcoderSolvedData, setAtcoderSolvedData] = React.useState<problemType[]>([]);
+  const [atcoderSolvedData, setAtcoderSolvedData] = React.useState<
+    problemType[]
+  >([]);
+  const [solvedProblemIds, setSolvedProblemIds] = React.useState<Set<string>>(
+    new Set()
+  );
 
   console.log({ userName });
 
@@ -20,6 +26,10 @@ function App() {
   const handleGetUserInfo = async () => {
     const res: problemType[] = await getAtCoderStatus(userName);
     setAtcoderSolvedData(res);
+    console.log(res);
+    setSolvedProblemIds(
+      new Set(res.filter((r) => r.result === 'AC').map((r) => r.problem_id))
+    );
   };
 
   React.useEffect(() => {}, []);
@@ -33,9 +43,33 @@ function App() {
         <div>
           こんにちは{userName} さん
           <br />
-          <Button variant="contained" color="secondary" onClick={handleGetUserInfo}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleGetUserInfo}
+          >
             apiを叩く
           </Button>
+          {problems.map((problem) => {
+            const endPoint = problem.url.split('/').splice(-1)[0];
+            const isAtcoder = problem.url.includes('atcoder.jp');
+            if (!isAtcoder) {
+              return <p>{problem.problem_id}-AtCoderの問題ではありません。</p>;
+            }
+            if (solvedProblemIds.has(endPoint)) {
+              return (
+                <p>
+                  {problem.problem_id}-{problem.title}解いています
+                </p>
+              );
+            } else {
+              return (
+                <p>
+                  {problem.problem_id}-{problem.title}解いてません
+                </p>
+              );
+            }
+          })}
         </div>
       )}
     </div>
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,586 +1,494 @@
+type workbook = {
+  problem_id: number
+  title: string
+  url: string
+}
 
-export const problems: any[] =
+export const problems: workbook[] =
 [
   {
     "problem_id": 1,
     "title": "ITP1_7_B - How Many Ways?",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_7_B&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 2,
     "title": "AtCoder Beginner Contest 106 B - 105",
     "url": "https://atcoder.jp/contests/abc106/tasks/abc106_b",
-    "description": ""
   },
   {
     "problem_id": 3,
     "title": "AtCoder Beginner Contest 122 B - ATCoder",
     "url": "https://atcoder.jp/contests/abc122/tasks/abc122_b",
-    "description": ""
   },
   {
     "problem_id": 4,
     "title": "\u30d1\u7814\u676f2019 C - \u30ab\u30e9\u30aa\u30b1",
     "url": "https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c",
-    "description": ""
   },
   {
     "problem_id": 5,
     "title": "AtCoder Beginner Contest 095 C - Half and Half",
     "url": "https://atcoder.jp/contests/abc095/tasks/arc096_a",
-    "description": ""
   },
   {
     "problem_id": 6,
     "title": "\u4e09\u4e95\u4f4f\u53cb\u4fe1\u8a17\u9280\u884c\u30d7\u30ed\u30b0\u30e9\u30df\u30f3\u30b0\u30b3\u30f3\u30c6\u30b9\u30c8 2019 D - Lucky PIN",
     "url": "https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_d",
-    "description": ""
   },
   {
     "problem_id": 7,
     "title": "JOI 2007 \u672c\u9078 3 - \u6700\u53e4\u306e\u907a\u8de1",
     "url": "https://atcoder.jp/contests/joi2007ho/tasks/joi2007ho_c",
-    "description": ""
   },
   {
     "problem_id": 8,
     "title": "Square869120Contest #6 B - AtCoder Markets",
     "url": "https://atcoder.jp/contests/s8pc-6/tasks/s8pc_6_b",
-    "description": ""
   },
   {
     "problem_id": 9,
     "title": "JOI 2008 \u4e88\u9078 4 - \u661f\u5ea7\u63a2\u3057",
     "url": "https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_d",
-    "description": ""
   },
   {
     "problem_id": 10,
     "title": "ALDS_5_A - \u7dcf\u5f53\u305f\u308a",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 11,
     "title": "AtCoder Beginner Contest 128 C - Switches",
     "url": "https://atcoder.jp/contests/abc128/tasks/abc128_c",
-    "description": ""
   },
   {
     "problem_id": 12,
     "title": "AtCoder Beginner Contest 002 D - \u6d3e\u95a5",
     "url": "https://atcoder.jp/contests/abc002/tasks/abc002_4",
-    "description": ""
   },
   {
     "problem_id": 13,
     "title": "JOI 2008 \u4e88\u9078 5 - \u304a\u305b\u3093\u3079\u3044",
     "url": "https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_e",
-    "description": ""
   },
   {
     "problem_id": 14,
     "title": "Square869120Contest #4 B - Buildings are Colorful!",
     "url": "https://atcoder.jp/contests/s8pc-4/tasks/s8pc_4_b",
-    "description": ""
   },
   {
     "problem_id": 15,
     "title": "AtCoder Beginner Contest 145 C - Average Length",
     "url": "https://atcoder.jp/contests/abc145/tasks/abc145_c",
-    "description": ""
   },
   {
     "problem_id": 16,
     "title": "AtCoder Beginner Contest 150 C - Count Order",
     "url": "https://atcoder.jp/contests/abc150/tasks/abc150_c",
-    "description": ""
   },
   {
     "problem_id": 17,
     "title": "ALDS_13_A - 8 \u30af\u30a4\u30fc\u30f3\u554f\u984c",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_13_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 18,
     "title": "ALDS_4_B - \u4e8c\u5206\u63a2\u7d22",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 19,
     "title": "JOI 2009 \u672c\u9078 2 - \u30d4\u30b6",
     "url": "https://atcoder.jp/contests/joi2009ho/tasks/joi2009ho_b",
-    "description": ""
   },
   {
     "problem_id": 20,
     "title": "AtCoder Beginner Contest 077 C - Snuke Festival",
     "url": "https://atcoder.jp/contests/abc077/tasks/arc084_a",
-    "description": ""
   },
   {
     "problem_id": 21,
     "title": "AtCoder Beginner Contest 023 D - \u5c04\u6483\u738b",
     "url": "https://atcoder.jp/contests/abc023/tasks/abc023_d",
-    "description": ""
   },
   {
     "problem_id": 22,
     "title": "AtCoder Regular Contest 054 B - \u30e0\u30fc\u30a2\u306e\u6cd5\u5247",
     "url": "https://atcoder.jp/contests/arc054/tasks/arc054_b",
-    "description": ""
   },
   {
     "problem_id": 23,
     "title": "\u4e09\u5206\u63a2\u7d22",
     "url": "https://qiita.com/ganariya/items/1553ff2bf8d6d7789127",
-    "description": ""
   },
   {
     "problem_id": 24,
     "title": "ALDS_11_B - \u6df1\u3055\u512a\u5148\u63a2\u7d22",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B",
-    "description": ""
   },
   {
     "problem_id": 25,
     "title": "AOJ 1160 - \u5cf6\u306f\u3044\u304f\u3064\u3042\u308b\uff1f",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1160&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 26,
     "title": "AtCoder Beginner Contest 138 D - Ki",
     "url": "https://atcoder.jp/contests/abc138/tasks/abc138_d",
-    "description": ""
   },
   {
     "problem_id": 27,
     "title": "JOI 2009 \u4e88\u9078 4 - \u8584\u6c37\u6e21\u308a",
     "url": "https://atcoder.jp/contests/joi2009yo/tasks/joi2009yo_d",
-    "description": ""
   },
   {
     "problem_id": 28,
     "title": "ALDS_11_C - \u5e45\u512a\u5148\u63a2\u7d22",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 29,
     "title": "AtCoder Beginner Contest 007 C - \u5e45\u512a\u5148\u63a2\u7d22",
     "url": "https://atcoder.jp/contests/abc007/tasks/abc007_3",
-    "description": ""
   },
   {
     "problem_id": 30,
     "title": "JOI 2011 \u4e88\u9078 5 - \u30c1\u30fc\u30ba",
     "url": "https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_e",
-    "description": ""
   },
   {
     "problem_id": 31,
     "title": "JOI 2012 \u4e88\u9078 5 - \u30a4\u30eb\u30df\u30cd\u30fc\u30b7\u30e7\u30f3",
     "url": "https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_e",
-    "description": ""
   },
   {
     "problem_id": 32,
     "title": "AOJ 1166 - \u8ff7\u56f3\u3068\u547d\u305a",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1166&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 33,
     "title": "AtCoder Beginner Contest 088 D - Grid Repainting",
     "url": "https://atcoder.jp/contests/abc088/tasks/abc088_d",
-    "description": ""
   },
   {
     "problem_id": 34,
     "title": "ALDS_10_A - \u30d5\u30a3\u30dc\u30ca\u30c3\u30c1\u6570",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 35,
     "title": "DPL_1_B - 0,1\u30ca\u30c3\u30d7\u30b6\u30c3\u30af\u554f\u984c",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 36,
     "title": "DPL_1_C - \u30ca\u30c3\u30d7\u30b6\u30c3\u30af\u554f\u984c",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 37,
     "title": "DPL_1_A - \u30b3\u30a4\u30f3\u554f\u984c",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 38,
     "title": "ALDS_10_C - \u6700\u9577\u5171\u901a\u90e8\u5206\u5217",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 39,
     "title": "JOI 2011 \u4e88\u9078 4 - 1 \u5e74\u751f",
     "url": "https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d",
-    "description": ""
   },
   {
     "problem_id": 40,
     "title": "JOI 2012 \u4e88\u9078 4 - \u30d1\u30b9\u30bf",
     "url": "https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_d",
-    "description": ""
   },
   {
     "problem_id": 41,
     "title": "JOI 2013 \u4e88\u9078 4 - \u6691\u3044\u65e5\u3005",
     "url": "https://atcoder.jp/contests/joi2013yo/tasks/joi2013yo_d",
-    "description": ""
   },
   {
     "problem_id": 42,
     "title": "JOI 2015 \u4e88\u9078 4 - \u30b7\u30eb\u30af\u30ed\u30fc\u30c9",
     "url": "https://atcoder.jp/contests/joi2015yo/tasks/joi2015yo_d",
-    "description": ""
   },
   {
     "problem_id": 43,
     "title": "\u30d1\u7814\u676f2019 D - \u30d1\u7814\u8ecd\u65d7",
     "url": "https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_d",
-    "description": ""
   },
   {
     "problem_id": 44,
     "title": "AOJ 1167 - \u30dd\u30ed\u30c3\u30af\u4e88\u60f3",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1167&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 45,
     "title": "AOJ 2199 - \u5dee\u5206\u30d1\u30eb\u30b9\u7b26\u53f7\u5909\u8abf",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2199&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 46,
     "title": "ALDS_10_B - \u9023\u9396\u884c\u5217\u7a4d",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_B&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 47,
     "title": "JOI 2015 \u672c\u9078 2 - \u30b1\u30fc\u30ad\u306e\u5207\u308a\u5206\u3051 2",
     "url": "https://atcoder.jp/contests/joi2015ho/tasks/joi2015ho_b",
-    "description": ""
   },
   {
     "problem_id": 48,
     "title": "AOJ 1611 \u30c0\u30eb\u30de\u843d\u3068\u3057",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1611&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 49,
     "title": "DPL_2_A - \u5de1\u56de\u30bb\u30fc\u30eb\u30b9\u30de\u30f3\u554f\u984c",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 50,
     "title": "Square869120Contest #1 G - Revenge of Traveling Salesman Problem",
     "url": "https://atcoder.jp/contests/s8pc-1/tasks/s8pc_1_g",
-    "description": ""
   },
   {
     "problem_id": 51,
     "title": "JOI 2014 \u4e88\u9078 4 - \u90e8\u6d3b\u306e\u30b9\u30b1\u30b8\u30e5\u30fc\u30eb\u8868",
     "url": "https://atcoder.jp/contests/joi2014yo/tasks/joi2014yo_d",
-    "description": ""
   },
   {
     "problem_id": 52,
     "title": "JOI 2017 \u4e88\u9078 4 - \u306c\u3044\u3050\u308b\u307f\u306e\u6574\u7406",
     "url": "https://atcoder.jp/contests/joi2017yo/tasks/joi2017yo_d",
-    "description": ""
   },
   {
     "problem_id": 56,
     "title": "GRL_1_A - \u5358\u4e00\u59cb\u70b9\u6700\u77ed\u7d4c\u8def",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 57,
     "title": "JOI 2008 \u4e88\u9078 6 - \u8239\u65c5",
     "url": "https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_f",
-    "description": ""
   },
   {
     "problem_id": 58,
     "title": "JOI 2016 \u4e88\u9078 5 - \u30be\u30f3\u30d3\u5cf6",
     "url": "https://atcoder.jp/contests/joi2016yo/tasks/joi2016yo_e",
-    "description": ""
   },
   {
     "problem_id": 59,
     "title": "JOI 2014 \u4e88\u9078 5 - \u30bf\u30af\u30b7\u30fc",
     "url": "https://atcoder.jp/contests/joi2014yo/tasks/joi2014yo_e",
-    "description": ""
   },
   {
     "problem_id": 60,
     "title": "GRL_1_C - \u5168\u70b9\u5bfe\u9593\u6700\u77ed\u7d4c\u8def",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 61,
     "title": "AtCoder Beginner Contest 012 D - \u30d0\u30b9\u3068\u907f\u3051\u3089\u308c\u306a\u3044\u904b\u547d",
     "url": "https://atcoder.jp/contests/abc012/tasks/abc012_4",
-    "description": ""
   },
   {
     "problem_id": 62,
     "title": "AtCoder Beginner Contest 079 D - Wall",
     "url": "https://atcoder.jp/contests/abc079/tasks/abc079_d",
-    "description": ""
   },
   {
     "problem_id": 63,
     "title": "AtCoder Beginner Contest 074 D - Restoring Road Network",
     "url": "https://atcoder.jp/contests/abc074/tasks/arc083_b",
-    "description": ""
   },
   {
     "problem_id": 64,
     "title": "GRL_2_A - \u6700\u5c0f\u5168\u57df\u6728",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_2_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 65,
     "title": "JOI 2010 \u6625\u5408\u5bbf - Finals",
     "url": "https://atcoder.jp/contests/joisc2010/tasks/joisc2010_finals",
-    "description": ""
   },
   {
     "problem_id": 66,
     "title": "\u554f\u984cpdf",
     "url": "https://www.ioi-jp.org/camp/2010/2010-sp-tasks/2010-sp-day3_22.pdf",
-    "description": ""
   },
   {
     "problem_id": 67,
     "title": "AOJ 1127 - Building a Space Station",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1127",
-    "description": ""
   },
   {
     "problem_id": 68,
     "title": "NTL_1_A - \u7d20\u56e0\u6570\u5206\u89e3",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 69,
     "title": "AtCoder Beginner Contest 084 D - 2017-like Number",
     "url": "https://atcoder.jp/contests/abc084/tasks/abc084_d",
-    "description": ""
   },
   {
     "problem_id": 70,
     "title": "NTL_1_B - \u3079\u304d\u4e57",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 71,
     "title": "Square869120Contest #1 E - \u6563\u6b69",
     "url": "https://atcoder.jp/contests/s8pc-1/tasks/s8pc_1_e",
-    "description": ""
   },
   {
     "problem_id": 72,
     "title": "AtCoder Beginner Contest 034 C - \u7d4c\u8def",
     "url": "https://atcoder.jp/contests/abc034/tasks/abc034_c",
-    "description": ""
   },
   {
     "problem_id": 73,
     "title": "AtCoder Beginner Contest 145 D - Knight",
     "url": "https://atcoder.jp/contests/abc145/tasks/abc145_d",
-    "description": ""
   },
   {
     "problem_id": 74,
     "title": "AtCoder Beginner Contest 021 D - \u591a\u91cd\u30eb\u30fc\u30d7",
     "url": "https://atcoder.jp/contests/abc021/tasks/abc021_d",
-    "description": ""
   },
   {
     "problem_id": 75,
     "title": "AtCoder Beginner Contest 149 F - Surrounded Nodes",
     "url": "https://atcoder.jp/contests/abc149/tasks/abc149_f",
-    "description": ""
   },
   {
     "problem_id": 76,
     "title": "\u5168\u56fd\u7d71\u4e00\u30d7\u30ed\u30b0\u30e9\u30df\u30f3\u30b0\u738b\u6c7a\u5b9a\u6226\u672c\u6226 A - Abundant Resources",
     "url": "https://atcoder.jp/contests/nikkei2019-final/tasks/nikkei2019_final_a",
-    "description": ""
   },
   {
     "problem_id": 77,
     "title": "JOI 2010 \u672c\u9078 1 - \u65c5\u4eba",
     "url": "https://atcoder.jp/contests/joi2010ho/tasks/joi2010ho_a",
-    "description": ""
   },
   {
     "problem_id": 78,
     "title": "JOI 2011 \u672c\u9078 1 - \u60d1\u661f\u63a2\u67fb",
     "url": "https://atcoder.jp/contests/joi2011ho/tasks/joi2011ho1",
-    "description": ""
   },
   {
     "problem_id": 79,
     "title": "AtCoder Beginner Contest 106 D - AtCoder Express 2",
     "url": "https://atcoder.jp/contests/abc106/tasks/abc106_d",
-    "description": ""
   },
   {
     "problem_id": 80,
     "title": "GigaCode 2019 D - \u5bb6\u306e\u5efa\u8a2d",
     "url": "https://atcoder.jp/contests/gigacode-2019/tasks/gigacode_2019_d",
-    "description": ""
   },
   {
     "problem_id": 81,
     "title": "AtCoder Beginner Contest 014 C - AtColor",
     "url": "https://atcoder.jp/contests/abc014/tasks/abc014_3",
-    "description": ""
   },
   {
     "problem_id": 82,
     "title": "AOJ 2013 - \u5927\u5d0e",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2013",
-    "description": ""
   },
   {
     "problem_id": 83,
     "title": "JOI 2015 \u672c\u9078 1 - \u9244\u9053\u904b\u8cc3",
     "url": "https://atcoder.jp/contests/joi2015ho/tasks/joi2015ho_a",
-    "description": ""
   },
   {
     "problem_id": 84,
     "title": "JOI 2012 \u672c\u9078 4 - \u91d8",
     "url": "https://atcoder.jp/contests/joi2012ho/tasks/joi2012ho4",
-    "description": ""
   },
   {
     "problem_id": 85,
     "title": "DSL_1_A - \u4e92\u3044\u306b\u7d20\u306a\u96c6\u5408",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=ja",
-    "description": ""
   },
   {
     "problem_id": 86,
     "title": "AtCoder Beginner Contest 075 C - Bridge",
     "url": "https://atcoder.jp/contests/abc075/tasks/abc075_c?lang=ja",
-    "description": ""
   },
   {
     "problem_id": 87,
     "title": "AtCoder Beginner Contest 120 D - Decayed Bridge",
     "url": "https://atcoder.jp/contests/abc120/tasks/abc120_d",
-    "description": ""
   },
   {
     "problem_id": 88,
     "title": "JOI 2008 \u672c\u9078 1 - \u7881\u77f3\u306a\u3089\u3079",
     "url": "https://atcoder.jp/contests/joi2008ho/tasks/joi2008ho_a",
-    "description": ""
   },
   {
     "problem_id": 89,
     "title": "JOI 2013 \u672c\u9078 1 - \u96fb\u98fe",
     "url": "https://atcoder.jp/contests/joi2013ho/tasks/joi2013ho1",
-    "description": ""
   },
   {
     "problem_id": 90,
     "title": "Square869120Contest #5 B - Emblem",
     "url": "https://atcoder.jp/contests/s8pc-5/tasks/s8pc_5_b",
-    "description": ""
   },
   {
     "problem_id": 91,
     "title": "AtCoder Beginner Contest 144 D - Water Bottle",
     "url": "https://atcoder.jp/contests/abc144/tasks/abc144_d",
-    "description": ""
   },
   {
     "problem_id": 92,
     "title": "AOJ 1193 - \u9023\u9396\u6d88\u6ec5\u30d1\u30ba\u30eb",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1193&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 93,
     "title": "Square869120Contest #3 B - \u77f3\u843d\u3068\u3057\u30b2\u30fc\u30e0",
     "url": "https://atcoder.jp/contests/s8pc-3/tasks/s8pc_3_b",
-    "description": ""
   },
   {
     "problem_id": 94,
     "title": "AOJ 1149 - \u30b1\u30fc\u30ad\u30ab\u30c3\u30c8",
     "url": "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1149&lang=jp",
-    "description": ""
   },
   {
     "problem_id": 95,
     "title": "AtCoder Beginner Contest 149 B - Greedy Takahashi",
     "url": "https://atcoder.jp/contests/abc149/tasks/abc149_b",
-    "description": ""
   },
   {
     "problem_id": 96,
     "title": "\u8caa\u6b32\u6cd5\u30a2\u30eb\u30b4\u30ea\u30ba\u30e0",
     "url": "http://www2.kobe-u.ac.jp/%7Eky/da2/haihu04.pdf",
-    "description": ""
   },
   {
     "problem_id": 97,
     "title": "AtCoder Beginner Contest 139 D - ModSum",
     "url": "https://atcoder.jp/contests/abc139/tasks/abc139_d",
-    "description": ""
   },
   {
     "problem_id": 98,
     "title": "AtCoder Beginner Contest 150 D - Semi Common Multiple",
     "url": "https://atcoder.jp/contests/abc150/tasks/abc150_d",
-    "description": ""
   },
   {
     "problem_id": 99,
     "title": "\u4e09\u4e95\u4f4f\u53cb\u4fe1\u8a17\u9280\u884c\u30d7\u30ed\u30b0\u30e9\u30df\u30f3\u30b0\u30b3\u30f3\u30c6\u30b9\u30c82019 \u4e88\u9078 E - Colorful Hats 2",
     "url": "https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_e",
-    "description": ""
   },
   {
     "problem_id": 100,
     "title": "DDCC2020 \u4e88\u9078 D - Digit Sum Replace",
     "url": "https://atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_d",
-    "description": ""
   }
 ]
