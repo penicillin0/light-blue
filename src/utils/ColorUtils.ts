61a53ac2672f51d46171d8680eb9f1d662b39534
penicillin0 <ayahitonakamura@gmail.com>
2021-03-31 00:38:28 +0900

    色を追加

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -1,6 +1,7 @@
 export const COLOR = {
   PRIMERY_COLOR: "#90caf9",
   PRIMERY_LIGHT_COLOR: "#a6d4fa",
+  PRIMERY_HIGH_LIGHT_COLOR: "#E7F4F5",
   WHITE: "#fafafa",
   LIGHT_GREY: "#eeeeee",
   GREY: "#9e9e9e",
